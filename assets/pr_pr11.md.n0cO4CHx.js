705c4074ce7cd62cfa5a163e050c3d53364518f6
ningguoxu <ningguoxu123456@163.com>
2024-10-26 15:07:17 +0800

    Add gist folder content for VitePress

---
+++ assets/pr_pr11.md.n0cO4CHx.js
@@ -0,0 +1,8 @@
+import{_ as e,c as r,a0 as t,o as a}from"./chunks/framework.CGHvQLJz.js";const f=JSON.parse('{"title":"任务十二：画中画效果","description":"","frontmatter":{},"headers":[],"relativePath":"pr/pr11.md","filePath":"pr/pr11.md"}'),n={name:"pr/pr11.md"};function o(s,p,c,d,i,l){return a(),r("div",null,p[0]||(p[0]=[t(`<h1 id="任务十二-画中画效果" tabindex="-1">任务十二：画中画效果 <a class="header-anchor" href="#任务十二-画中画效果" aria-label="Permalink to &quot;任务十二：画中画效果&quot;">​</a></h1><p>1.新建项目→新建序列</p><p>2.编辑→首选项→时间轴→“视频切换默认持续时间”改为50帧（即2秒）→“静止图像默认持续时间”改为125 帧（即5秒）</p><p>3.文件→导入（“01-05”和“长背景”）</p><p>4.将“01-05”拖至V2，“02”和“04”在原位置上拖至V3</p><p>5.“长背景”拖至V1，与V2等长。</p><p>6.时间线上选择“01”，效果控件中缩放比例设为60</p><p>7.效果控件中，选中“01”的“运动”，按Ctrl+ C复制，时间线上依次选中“02-05”，按Ctrl+ V粘贴，缩放比例统一改为60</p><p>8.“长背景”→第0秒→位置前添加动画关键帧→位置（-100,238）→第24秒24帧→位置（800,288）→制作平移效果</p><p>9.“效果”窗口→视频过渡→擦除→带状擦除→拖至时间线“01”入点→选择“棋盘”→拖至“01”出点</p><p>10.同样</p><pre><code>“02”：入点“双侧平推门”，出点“棋盘擦除”
+
+“03”：入点“时钟式擦除”，出点“插入”
+
+“04”：入点“风车”，出点“随机块”
+
+“05”：入点“随机擦除”，出点“螺旋框”
+</code></pre><p>11.预览导出。</p>`,13)]))}const _=e(n,[["render",o]]);export{f as __pageData,_ as default};
